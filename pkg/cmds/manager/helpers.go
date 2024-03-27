7421a959782d7d2a6fc15e1e55cde8246202891d
Rokibul Hasan <mdrokibulhasan@appscode.com>
2024-03-27 09:53:00 +0600

    Add agentHealthProber
    Signed-off-by: Rokibul Hasan <mdrokibulhasan@appscode.com>

---
+++ pkg/cmds/manager/helpers.go
@@ -0,0 +1,51 @@
+package manager
+
+import (
+	"fmt"
+
+	"github.com/kluster-manager/cluster-auth/pkg/common"
+
+	agentapi "open-cluster-management.io/addon-framework/pkg/agent"
+	workapiv1 "open-cluster-management.io/api/work/v1"
+)
+
+// agentHealthProber returns an instance of the agent's health prober. It is used for
+// probing and checking the health status of the agent.
+func agentHealthProber() *agentapi.HealthProber {
+	return &agentapi.HealthProber{
+		Type: agentapi.HealthProberTypeWork,
+		WorkProber: &agentapi.WorkHealthProber{
+			ProbeFields: []agentapi.ProbeField{
+				{
+					ResourceIdentifier: workapiv1.ResourceIdentifier{
+						Group:     "apps",
+						Resource:  "deployments",
+						Name:      "cluster-auth-agent",
+						Namespace: common.AddonAgentInstallNamespace,
+					},
+					ProbeRules: []workapiv1.FeedbackRule{
+						{
+							Type: workapiv1.WellKnownStatusType,
+						},
+					},
+				},
+			},
+			HealthCheck: func(identifier workapiv1.ResourceIdentifier, result workapiv1.StatusFeedbackResult) error {
+				if len(result.Values) == 0 {
+					return fmt.Errorf("no values are probed for deployment %s/%s", identifier.Namespace, identifier.Name)
+				}
+				for _, value := range result.Values {
+					if value.Name != "ReadyReplicas" {
+						continue
+					}
+
+					if *value.Value.Integer >= 1 {
+						return nil
+					}
+					return fmt.Errorf("readyReplica is %d for deployement %s/%s", *value.Value.Integer, identifier.Namespace, identifier.Name)
+				}
+				return fmt.Errorf("readyReplica is not probed")
+			},
+		},
+	}
+}
